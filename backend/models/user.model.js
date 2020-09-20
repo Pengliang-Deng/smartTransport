3bda7bc1496046e2aa68ca4d2822fbce3e9d8a28
Zihan <zihanz.goodluck@gmail.com>
2020-09-20 12:57:47 +0800

    gameData schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -14,3 +14,45 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+const gameDataSchema = new mongoose.Schema({
+    fieldInfo: {
+        size: {
+            type: Number,
+            required: true
+        },
+        tileBackground: String,
+        fenceImage: String,
+        gridBackground: String,
+        gridOutline: String,
+
+        grids: [
+            {
+                flower: String,
+                status: String,
+                growthValue: Number,
+                waterValue: Number,
+            }
+        ],
+    },
+    playerInfo: {
+        playerName: String,
+        /* TODO */
+    },
+    itemsInfo: {
+        coins: Number,
+        resources: {
+            water: Number,
+            fertilizer: Number,
+            sunny: Number,
+        },
+        seeds: [
+            {
+                name: String,
+                amount: Number,
+            }
+        ]
+    }
+
+})
