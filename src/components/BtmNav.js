52a29d454e13a9fb2d67137544fe773422bb539d
邓鹏亮 <pengliangdeng@sukes-MacBook-puro.local>
2020-10-11 14:43:41 +1000

    btmNav fixed bottom

--- src/components/BtmNav.js
+++ src/components/BtmNav.js
@@ -3,13 +3,22 @@ import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import House from '@material-ui/icons/House';
 import Timer from '@material-ui/icons/Timer';
-import QuestionAnswer from '@material-ui/icons/QuestionAnswer'
-import { Link } from 'react-router-dom';
-import makeStyles from '@material-ui/core/styles/makeStyles'
+import QuestionAnswer from '@material-ui/icons/QuestionAnswer';
+import AppBar from '@material-ui/core/AppBar';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+
 
 const useStyles = makeStyles({
     container: {
-        marginTop: 80
+        marginTop: 0,
+    },
+    appbar: {
+        bottom: 0,
+        top: 'auto',
+        paddingTop: 16,
+        paddingBottom: 16,
+        backgroundColor: 'white'
     }
 })
 
@@ -31,23 +40,26 @@ export default function BtmNav(props) {
 
 
     return (
-        <BottomNavigation
-            value={value}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            showLabels
-            className={styles.container}
-        >
-        
-            <BottomNavigationAction onClick={trackNav} label="Tracker" icon={<Timer />} /> 
+        <AppBar className={styles.appbar} position={'fixed'}>
+            <BottomNavigation
+
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                showLabels
+                className={styles.container}
+            >
                 
-          
-            <BottomNavigationAction onClick={gardenNav} label="Garden" icon={<House />} />
-     
+                <BottomNavigationAction onClick={trackNav} label="Tracker" icon={<Timer />} /> 
+                    
+            
+                <BottomNavigationAction onClick={gardenNav} label="Garden" icon={<House />} />
+        
 
-            <BottomNavigationAction onClick={challengesNav} label="Challenges" icon={<QuestionAnswer />} />
+                <BottomNavigationAction onClick={challengesNav} label="Challenges" icon={<QuestionAnswer />} />
 
-        </BottomNavigation>
+            </BottomNavigation>
+        </AppBar>
     )
 }
